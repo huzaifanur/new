5cf119964994ee7578e3233c50e0d75fbd915b76
huzaifanur <huzaifa98nur@gmail.com>
2022-03-23 22:35:30 +0500

    WordLength Added

---
+++ src/components/Range.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export default function Range() {
+  const [value, setValue] = useState(5);
+
+  return (
+    <div className="my-3 w-80 bg-slate-200 p-4 rounded-md font-bold">
+      <div className="center h-full text-center ">World Length : {value}</div>
+      <div className="h-full">
+        <label htmlFor="wordLength" className="hidden"></label>
+        <input
+          onChange={(e) => setValue(e.target.value)}
+          id="wordLength"
+          type="range"
+          step={1}
+          min={4}
+          max={11}
+          name="price"
+          className="w-full h-2 bg-slate-600 appearance-none rounded-md"
+        />
+      </div>
+    </div>
+  );
+}
--- src/wordle/components/grid/Grid.jsx
+++ src/wordle/components/grid/Grid.jsx
@@ -1,7 +1,8 @@
-import { MAX_CHALLENGES } from '../../constants/settings'
-import { CompletedRow } from './CompletedRow'
-import { CurrentRow } from './CurrentRow'
-import { EmptyRow } from './EmptyRow'
+import Range from "../../../components/range";
+import { MAX_CHALLENGES } from "../../constants/settings";
+import { CompletedRow } from "./CompletedRow";
+import { CurrentRow } from "./CurrentRow";
+import { EmptyRow } from "./EmptyRow";
 
 // type Props = {
 //   guesses: string[]
@@ -19,23 +20,28 @@ export const Grid = ({
   const empties =
     guesses.length < MAX_CHALLENGES - 1
       ? Array.from(Array(MAX_CHALLENGES - 1 - guesses.length))
-      : []
+      : [];
 
   return (
-    <>
-      {guesses.map((guess, i) => (
-        <CompletedRow
-          key={i}
-          guess={guess}
-          isRevealing={isRevealing && guesses.length - 1 === i}
-        />
-      ))}
-      {guesses.length < MAX_CHALLENGES && (
-        <CurrentRow guess={currentGuess} className={currentRowClassName} />
-      )}
-      {empties.map((_, i) => (
-        <EmptyRow key={i} />
-      ))}
-    </>
-  )
-}
+    <div className="flex flex-col items-center">
+      <div>
+        <Range />
+      </div>
+      <div>
+        {guesses.map((guess, i) => (
+          <CompletedRow
+            key={i}
+            guess={guess}
+            isRevealing={isRevealing && guesses.length - 1 === i}
+          />
+        ))}
+        {guesses.length < MAX_CHALLENGES && (
+          <CurrentRow guess={currentGuess} className={currentRowClassName} />
+        )}
+        {empties.map((_, i) => (
+          <EmptyRow key={i} />
+        ))}
+      </div>
+    </div>
+  );
+};
